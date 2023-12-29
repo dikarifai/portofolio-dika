96aa2ac7a7cccad519661db11d9892451585b02a
Dika Rifai <twdrr02@gmail.com>
2023-12-29 23:44:15 +0800

    responsive home page

--- src/Layout/PortofolioLayout.jsx
+++ src/Layout/PortofolioLayout.jsx
@@ -6,7 +6,7 @@ const PortofolioLayout = () => {
   return (
     <div className="bg-[#E1DCE0] aspect-auto flex flex-row overflow-hidden">
       <Sidebar />
-      <div className="w-full bg-[#D7D7D7] flex h-screen overflow-auto">
+      <div className="w-full bg-[#D7D7D7] flex h-screen overflow-auto md-max:relative">
         <Outlet />
       </div>
     </div>
