92bd8705157c3033b92de9a90f1a065a6c097394
Dika Rifai <twdrr02@gmail.com>
2024-01-23 10:18:01 +0800

    add project

--- src/data/portofolioData.js
+++ src/data/portofolioData.js
@@ -1,8 +1,17 @@
 import bukapedia from "../img/Bukapedia.png";
+import ngabsign from "../img/ngabsing.png";
 
 export const portofolioData = [
   {
     id: 1,
+    name: "Ngabsign",
+    tech: ["React.js", "Tailwind"],
+    url: "https://ngabsign.id/",
+    img: ngabsign,
+    desc: "Ngabsign adalah aplikasi absensi karyawan untuk digunakan oleh perusahaan. Saya bergabung pada tim ini dikarenakan frontend developer dari ngabsign sendiri tidak dapat melanjutkan project ini, jadi saya diajak untuk mengembangkan project ini. Tugas saya disini adalah mengembangkan fitur dan memperbaiki bug pada project di sisi frontend pada web absensi untuk karyawan, ",
+  },
+  {
+    id: 2,
     name: "Bukapedia",
     tech: ["Vite.js", "Redux Toolkit", "Redux Thunk"],
     api: "https://fakestoreapi.com/",
